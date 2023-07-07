17c0bfdc1694bbf0abcd50d462af2aa4989a3542
Andressa Bendette <andressa@lumosweb.com.br>
2023-07-07 13:00:28 -0300

    fix: error script img file

--- watch-img.js
+++ watch-img.js
@@ -1,5 +1,6 @@
 const chokidar = require('chokidar');
 const fs = require('fs-extra');
+const path = require('path');
 
 const srcPath = 'src/img';
 const distPath = 'dist/img';
@@ -8,12 +9,13 @@ const watcher = chokidar.watch(srcPath, { ignored: /^\./, persistent: true });
 
 console.log(`Watching ${srcPath} for new images...`);
 
-watcher.on('add', async (path) => {
-  const fileName = path.split('/').pop();
-  const distFilePath = `${distPath}/${fileName}`;
-  
+watcher.on('add', async (filePath) => {
+  const fileName = path.basename(filePath);
+  const distFilePath = path.join(distPath, fileName);
+
   try {
-    await fs.copy(path, distFilePath);
+    await fs.ensureDir(distPath); // Create the distPath directory if it doesn't exist
+    await fs.copy(filePath, distFilePath);
   } catch (err) {
     console.error(`Error copying ${fileName}:`, err);
   }
